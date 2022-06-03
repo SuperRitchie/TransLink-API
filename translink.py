b5a39469b6027e56e2a742d4533560418f6432bb
Ritchie <flamener@hotmail.com>
2022-06-03 00:27:57 -0700

    add hover marker

--- translink.py
+++ translink.py
@@ -1,50 +1,57 @@
-from urllib import response
 import requests
-import json
 from decouple import config
 import gmplot
+import time
 
 APIkey = config('KEY')
+mapAPI = config('gmapKEY')
 url = "https://api.translink.ca/rttiapi/v1/buses?apikey=" + APIkey # + "&stopNo=53987"
 header = {'content-type': 'application/json', 'accept': 'text/plain'}
 r = requests.get(url, headers=header)
 data = r.json()
-#print(data)
-#print all of the routes in the data of the API
-#print routes in the data variable
 
-#generate a random number between 1 and 20
-#print the route number of the random number
-
-#generate a random number between 1 and 20
-#print the stop number of the random number
-
-def printStop(data):
+def getLocation(data):
     latitudes = []
     longitudes = []
-    #store all of the longitudes in the longitudes list
-    count = 0
+    destinations = []
+    directions = []
+    times = []
+    routesNo = []
+    
     for bus in data:
         latitudes.append(float(bus['Latitude']))
         longitudes.append(float(bus['Longitude']))
-        count += 1
-        #print(bus)
-        #convert latitudes to float and store in latitudes
-        #print bus latitude
-        #print(bus['Latitude'])
+        destinations.append(bus['Destination'])
+        directions.append(bus['Direction'])
+        times.append(bus['RecordedTime'])
+        routesNo.append(bus['RouteNo'])
         
-            
-        #print(bus)
-        #print("Bus " + bus['RouteNo'])
-    #print(count)
-    return latitudes, longitudes
-#printStop(data)
-latitudes, longitudes = printStop(data)
-print(latitudes)
-print(longitudes)
-gmap3 = gmplot.GoogleMapPlotter(49, -123, 13)
-gmap3.scatter(latitudes, longitudes, '#FF0000', size = 40, marker = False)
+        #only append for buses numbers between 7000 and 7999
+        
+        '''
+        if(bus['VehicleNo'] >= '7000' and bus['VehicleNo'] <= '7999'):
+            #print(bus)
+            latitudes.append(float(bus['Latitude']))
+            longitudes.append(float(bus['Longitude']))
+            destinations.append(bus['Destination'])
+            directions.append(bus['Direction'])
+            times.append(bus['RecordedTime'])
+            routesNo.append(bus['RouteNo'])
+        '''
+    return latitudes, longitudes, destinations, directions, times, len(latitudes), routesNo
 
+latitudes, longitudes, destinations, directions, times, length, routesNo = getLocation(data)
+averageLat = sum(latitudes)/len(latitudes)
+averageLong = sum(longitudes)/len(longitudes)
+print(averageLat), print(averageLong)
+
+gmap3 = gmplot.GoogleMapPlotter(averageLat, averageLong, 13, apikey=mapAPI)
+gmap3.scatter(latitudes, longitudes, '#ff0000')
+for i in range(length):
+    time.sleep(0.001)
+    gmap3.marker(latitudes[i], longitudes[i], title = routesNo[i] + " " + destinations[i] + " " + directions[i] + " " + times[i])
+
+time.sleep(5)
 gmap3.draw("C:\\Users\\Ritchie\\TransLink-API\\map.html")
 
 #print(r.headers['content-type'])
